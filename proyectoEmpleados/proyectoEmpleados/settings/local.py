3f3872f05a6353a02404652593ea7e21416e77c4
sren94 <stroop94@gmail.com>
2024-04-28 10:40:42 -0600

    12avo commit

--- proyectoEmpleados/proyectoEmpleados/settings/local.py
+++ proyectoEmpleados/proyectoEmpleados/settings/local.py
@@ -1,4 +1,16 @@
 from .base import *
+from pathlib import Path
+import environ  # <-- Updated!
+environ.Env.read_env(BASE_DIR / '.env')  # <-- Updated!
+env = environ.Env(  # <-- Updated!
+    # set casting, default value
+    DEBUG=(bool, False),
+)
+SECRET_KEY = env('SECRET_KEY')  # <-- Updated!
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG') 
+
 DEBUG = True
 import os
 ALLOWED_HOSTS = []
@@ -7,15 +19,16 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     #}
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'dbEmpleado',
-        'USER':'stroop94',
-        'PASSWORD':'Stroop94',
-        'HOST':'localhost',
-        'PORT':'5432',
-        
-}
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME':'dbEmpleado',
+#        'USER':'stroop94',
+#        'PASSWORD':'Stroop94',
+#        'HOST':'localhost',
+#        'PORT':'5432',
+#        
+#}  
+environ.Env.read_env(BASE_DIR / '.env')  # <-- Updated!
 }
 #aqui se registra para la creacion de archivos estaticos
 #como css,js etc 
@@ -29,3 +42,5 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
 #se deben de registrar en esta carpeta
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+environ.Env.read_env(BASE_DIR / '.env')  # <-- Updated!
