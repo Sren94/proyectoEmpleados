fa341ba5905cc277a11429d7f63717c0e083e749
sren94 <stroop94@gmail.com>
2024-10-23 10:37:47 -0600

    Commit 2.0.5 Fv

--- proyectoEmpleados/proyectoEmpleados/settings/local.py
+++ proyectoEmpleados/proyectoEmpleados/settings/local.py
@@ -1,24 +1,20 @@
 from .base import *
+from pathlib import Path
+import environ  # <-- Updated!
 import os
-import environ
+environ.Env.read_env(BASE_DIR / '.env')  # <-- Updated!
+env = environ.Env(  # <-- Updated!
+    # set casting, default value
+    DEBUG=(bool, False),
+)
+SECRET_KEY = env('SECRET_KEY')  # <-- Updated!
 
-# Cargar las variables de entorno
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG') 
 
-# Clave secreta y configuración de depuración
-SECRET_KEY = env('SECRET_KEY')
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = True
 
-# Hosts permitidos
 ALLOWED_HOSTS = []
-
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
-#Media files
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DATABASES = {
     'default': {
@@ -30,3 +26,18 @@ DATABASES = {
         'PORT':'5432',
     }
 }
+#aqui se registra para la creacion de archivos estaticos
+#como css,js etc 
+#y sirve para obtener y direccionar los recursos 
+#para el proyecto
+STATIC_URL = 'static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
+
+#aqui se da registro de los archivos media 
+#y esto sirve para direccionar los recursos
+#se deben de registrar en esta carpeta
+MEDIA_URL='media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+environ.Env.read_env(BASE_DIR / '.env')  # <-- Updated!
+STATIC_URL = 'static/'
