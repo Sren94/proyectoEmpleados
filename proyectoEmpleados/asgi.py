3e70d03b8ffd09afd907ea12ae346e104cdd8350
sren94 <stroop94@gmail.com>
2024-11-03 21:11:58 -0600

    V main 1.0.1

--- proyectoEmpleados/asgi.py
+++ proyectoEmpleados/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoEmpleados.settings.local')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoEmpleados.settings.prod')
+
 
 application = get_asgi_application()
